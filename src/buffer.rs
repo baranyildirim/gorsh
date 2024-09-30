909aa1417aff15e127888495839d66a0c463586f
baranyildirim@gmail.com <baran.yildirim@outlook.com>
2024-09-30 01:15:07 +0000

    buffer.rs: unittests

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,13 +1,13 @@
-use bytes::BufMut;
 use bytes::Bytes;
 use bytes::BytesMut;
+use std::cmp::min;
 use std::collections::LinkedList;
 use std::sync::Condvar;
 use std::sync::Mutex;
 
 #[derive(Default)]
 struct BufferInner {
-    elements: LinkedList<Bytes>,
+    elements: LinkedList<BytesMut>,
     closed: bool,
 }
 
@@ -24,7 +24,7 @@ impl Buffer {
         }
     }
 
-    pub fn write(&mut self, bytes: Bytes) {
+    pub fn write(&mut self, bytes: BytesMut) {
         let mut guard = self.inner.lock().unwrap();
         guard.elements.push_back(bytes);
         self.cond.notify_one();
@@ -36,28 +36,87 @@ impl Buffer {
         self.cond.notify_one();
     }
 
-    pub fn read(&mut self) -> Result<BytesMut, crate::Error> {
+    pub fn read(&mut self, size: usize) -> Result<Bytes, crate::Error> {
         let mut guard = self.inner.lock().unwrap();
-        let mut bytes = BytesMut::new();
-        loop {
-            if let Some(head) = &guard.elements.pop_front() {
-                bytes.extend_from_slice(head.as_ref());
+        let mut bytes = BytesMut::with_capacity(size);
+        while bytes.len() < size {
+            if let Some(head) = guard.elements.front_mut() {
+                let split_size = min(size - bytes.len(), head.len());
+                bytes.extend(head.split_to(split_size));
+                if head.is_empty() {
+                    guard.elements.pop_front();
+                }
                 continue;
             }
 
             if !bytes.is_empty() {
-                return Ok(bytes);
+                break;
             }
 
             if guard.closed {
                 return Err(crate::Error::EOF);
             }
+
             guard = self.cond.wait(guard).unwrap();
         }
+        Ok(bytes.freeze())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use bytes::BufMut;
+
     use super::*;
+
+    #[test]
+    fn test_buffer_read_write_at_capacity() {
+        let mut b = Buffer::new();
+        let mut to_write = BytesMut::with_capacity(1);
+        to_write.put_u8(1);
+        b.write(to_write);
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[1]);
+    }
+
+    #[test]
+    fn test_buffer_read_write_exceeds_capacity() {
+        let mut b = Buffer::new();
+        let mut to_write = BytesMut::with_capacity(1);
+        to_write.put_u8(1);
+        to_write.put_u8(2);
+        b.write(to_write);
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[1]);
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[2]);
+    }
+
+    #[test]
+    fn test_buffer_read_write_multi_write() {
+        let mut b = Buffer::new();
+        let mut to_write = BytesMut::with_capacity(1);
+        to_write.put_u8(1);
+        b.write(to_write);
+        let mut to_write = BytesMut::with_capacity(1);
+        to_write.put_u8(2);
+        b.write(to_write);
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[1]);
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[2]);
+    }
+
+    #[test]
+    fn test_buffer_read_write_close() {
+        let mut b = Buffer::new();
+        let mut to_write = BytesMut::with_capacity(1);
+        to_write.put_u8(1);
+        b.write(to_write);
+        b.eof();
+        let read_bytes = b.read(1).unwrap();
+        assert_eq!(read_bytes.as_ref(), &[1]);
+        let eof_error = b.read(1).unwrap_err();
+        assert_eq!(eof_error, crate::Error::EOF);
+    }
 }
